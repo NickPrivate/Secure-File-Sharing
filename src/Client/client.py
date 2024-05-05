6ee9573df7d8acb1d4bbdc68ecc643eabb49cca4
NickPrivate <dragonwolf478@gmail.com>
2024-05-04 23:26:27 -0700

    Added key generation on client side, added a function on the server to receive messages over 1024 bytes, when a user registers a new account new DSA/RSA keys are generated and stored in the clients directory

--- src/Client/client.py
+++ src/Client/client.py
@@ -1,4 +1,8 @@
 from Crypto.Cipher import AES
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import dsa
+from cryptography.hazmat.primitives import serialization
+
 import os
 import socket
 import time
@@ -20,9 +24,88 @@ def receive_full_message(sock):
             break
     return b''.join(buffer)
 
+
+def generate_dsa_keys():
+    private_key = dsa.generate_private_key(
+        key_size=2048
+    )
+
+    pem_private_key = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+    public_key = private_key.public_key()
+
+    pem_public_key = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    return pem_private_key.decode('utf-8'), pem_public_key.decode('utf-8')
+
+def generate_rsa_keys():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+
+    public_key = private_key.public_key()
+
+    pem_private = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+    pem_public = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    return pem_private.decode('utf-8'), pem_public.decode('utf-8')
+
+
+def load_or_generate_keys():
+    rsa_pub_path = 'rsa_public.pem'
+    rsa_priv_path = 'rsa_public.pem'
+    dsa_pub_path = 'dsa_public.pem'
+    dsa_priv_path = 'dsa_public.pem'
+    '''if os.path.exists(rsa_pub_path) and os.path.exists(dsa_pub_path):
+        with open(rsa_pub_path, 'r') as f:
+            RSApublic_key = f.read()
+        with open(dsa_pub_path, 'r') as f:
+            DSApublic_key = f.read()'''
+    RSAprivate_key, RSApublic_key = generate_rsa_keys()
+    DSAprivate_key, DSApublic_key = generate_dsa_keys()
+    with open(rsa_pub_path, 'w') as f:
+        f.write(RSApublic_key)
+    with open(dsa_pub_path, 'w') as f:
+        f.write(DSApublic_key)
+    with open(rsa_priv_path, 'w') as f:
+        f.write(RSAprivate_key)
+    with open(dsa_priv_path, 'w') as f:
+        f.write(DSAprivate_key)
+
+    return RSApublic_key, DSApublic_key, RSAprivate_key, DSAprivate_key
+
+RSApublic_key, DSApublic_key, RSAprivate_key, DSAprivate_key = load_or_generate_keys()
+
 try:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
+
+    client.send(RSApublic_key.encode('utf-8'))
+    client.recv(1024).decode()
+    client.send(DSApublic_key.encode('utf-8'))
+
+    keys_sent_ack = client.recv(1024).decode()
+    if keys_sent_ack == "Both Keys Received":
+        print("Server acknowledged keys, proceeding...")
+    else:
+        print("Did not receive correct acknowledgment from server, exiting...")
+        client.close()
 except:
     print("Connection Failed, Server is not Running")
     exit()
@@ -36,19 +119,19 @@ try:
             while True:
                 response = input().strip()
                 if response:
-                    client.send(response.encode())
+                    client.send(response.encode('utf-8'))
                     break
                 else:
                     print("No input detected")
         elif message == file_kw:
             response = input().strip()
-            client.send(response.encode())
+            client.send(response.encode('utf-8'))
         elif message == file_send_string:
-            # TODO Create a function for the client that sends the file
+            # TODO Create a function receiver() for the client that sends the file
             print("THIS IS WHERE SEND LOGIC GOES")
             break
         elif message == file_request_string:
-            # TODO Create a function for the client that receives the senders file 
+            # TODO Create a function sender() for the client that receives the senders file 
             print("THIS IS WHERE REQUEST LOGIC GOES")
             break
         else:
--- src/Server/server.py
+++ src/Server/server.py
@@ -1,7 +1,4 @@
 from prettytable import PrettyTable
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives.asymmetric import dsa
-from cryptography.hazmat.primitives import serialization
 
 import socket
 import hashlib
@@ -12,53 +9,11 @@ HOST = '127.0.0.1'
 PORT = 9999
 
 
-def generate_dsa_keys():
-    private_key = dsa.generate_private_key(
-        key_size=2048
-    )
-
-    pem_private_key = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
-    )
-
-    public_key = private_key.public_key()
-
-    pem_public_key = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-
-    return pem_private_key.decode('utf-8'), pem_public_key.decode('utf-8')
-
-def generate_rsa_keys():
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-    )
-
-    public_key = private_key.public_key()
-
-    pem_private = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
-    )
-
-    pem_public = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-
-    return pem_private.decode('utf-8'), pem_public.decode('utf-8')
-
-
 def domain_handle(client_socket, cur, user_id):
     while True:
-        client_socket.send("To upload files please enter your domain name (IP Address): ".encode())
+        client_socket.send("To upload files please enter your domain name (IP Address): ".encode('utf-8'))
         domain = client_socket.recv(1024).decode().strip()
-        client_socket.send("Please enter your port number: ".encode())
+        client_socket.send("Please enter your port number: ".encode('utf-8'))
         port = client_socket.recv(1024).decode().strip()
 
         try:
@@ -66,7 +21,7 @@ def domain_handle(client_socket, cur, user_id):
             if not 0 < port < 65536:
                 raise ValueError("Port number must be between 1 and 65535")
         except ValueError as e:
-            client_socket.send(f"Invalid port number: {e}\n".encode())
+            client_socket.send(f"Invalid port number: {e}\n".encode('utf-8'))
             continue
 
         cur.execute("SELECT * FROM Peers WHERE user_id=?", (user_id,))
@@ -77,94 +32,93 @@ def domain_handle(client_socket, cur, user_id):
             cur.execute("INSERT INTO Peers (user_id, domain_name, port) VALUES (?, ?, ?)", (user_id, domain, port))
             break
     cur.connection.commit()
-    client_socket.send("Domain and port updated successfully!\n".encode())
+    client_socket.send("Domain and port updated successfully!\n".encode('utf-8'))
     print(f"UserID: {user_id} | Domain: {domain} | Port: {port}")
 
 
-def handle_registration(client_socket, cur):
+def handle_registration(client_socket, cur, clientRSA, clientDSA):
     while True:
-        client_socket.send("Enter New Username: ".encode())
+        client_socket.send("Enter New Username: ".encode('utf-8'))
         username = client_socket.recv(1024).decode().strip()
         if not username:
             continue
-        client_socket.send("Enter New Password: ".encode())
+        client_socket.send("Enter New Password: ".encode('utf-8'))
         password = client_socket.recv(1024).decode().strip()
         if not password:
             continue
-        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
 
         cur.execute("SELECT * FROM Users WHERE username=?", (username,))
         if cur.fetchone() is not None:
-            client_socket.send("Username already exists. Try a different username.\n".encode())
+            client_socket.send("Username already exists. Try a different username.\n".encode('utf-8'))
         else:
-            DSAprivate_key, DSApublic_key = generate_dsa_keys()
-            RSAprivate_key, RSApublic_key = generate_rsa_keys()
-            cur.execute("INSERT INTO Users (username, password, RSApublickey, DSApublickey) VALUES (?, ?, ?, ?)", (username, hashed_password, RSApublic_key, DSApublic_key ))
+            cur.execute("INSERT INTO Users (username, password, RSApublickey, DSApublickey) VALUES (?, ?, ?, ?)", (username, hashed_password, clientRSA, clientDSA))
             cur.connection.commit()
             user_id = cur.lastrowid
             cur.execute("INSERT INTO Peers (user_id, domain_name, port) VALUES (?, NULL, NULL)", (user_id,))
             cur.connection.commit()
-            client_socket.send("Registration successful!\n".encode())
-            """client_socket.send(f"Your private key is: {private_key}".encode())"""
+            client_socket.send("Registration successful!\n".encode('utf-8'))
+            """client_socket.send(f"Your private key is: {private_key}".encode('utf-8'))"""
             print(f"UserID: {user_id} | Registered Username: {username} | Registered Password hash: {password}")
             return user_id
 
 
-def handle_login(client_socket, cur):
+def handle_login(client_socket, cur, clientRSA, clientDSA):
     while True:
-        client_socket.send("Enter Username: ".encode())
+        client_socket.send("Enter Username: ".encode('utf-8'))
         username = client_socket.recv(1024).decode().strip()
         if not username:
             continue
-        client_socket.send("Enter Password: ".encode())
+        client_socket.send("Enter Password: ".encode('utf-8'))
         password = client_socket.recv(1024).decode().strip()
         if not password:
             continue
-        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
 
         cur.execute("SELECT * FROM Users WHERE username=? AND password=?", (username, hashed_password))
         result = cur.fetchone()
         if result: 
+#            cur.execute("UPDATE Users SET RSApublickey=?, DSApublickey=? WHERE username=?", (clientRSA, clientDSA, username))
             user_id = result[0]
-            client_socket.send("Login Successful!\n".encode())
+            client_socket.send("Login Successful!\n".encode('utf-8'))
             print(f"UserID: {user_id} | Registered Username: {username} | Registered Password hash: {password}")
             return user_id
         else:
-            client_socket.send("Login Failed. Try again.\n".encode())
+            client_socket.send("Login Failed. Try again.\n".encode('utf-8'))
 
 
 def upload_files(client_socket, cur, user_id):
     while True:
-        client_socket.send("Enter the number of files you want to upload: ".encode())
+        client_socket.send("Enter the number of files you want to upload: ".encode('utf-8'))
         num_of_files = client_socket.recv(1024).decode().strip()
 
         if not num_of_files.isdigit():
-            client_socket.send("Error: enter a number".encode())
+            client_socket.send("Error: enter a number".encode('utf-8'))
             continue
 
         num_of_files = int(num_of_files)
         for i in range (1, num_of_files + 1):
-            client_socket.send(f"Enter the name of file {i}:".encode())
+            client_socket.send(f"Enter the name of file {i}:".encode('utf-8'))
             file_name = client_socket.recv(1024).decode().strip()
             if not file_name:
-                client_socket.send("Error file cannot be empty\n".encode())
+                client_socket.send("Error file cannot be empty\n".encode('utf-8'))
                 continue
 
-            client_socket.send(f"Enter the keyword of file {i}:".encode())
+            client_socket.send(f"Enter the keyword of file {i}:".encode('utf-8'))
             keyword_name = client_socket.recv(1024).decode().strip()
             if not keyword_name:
-                client_socket.send("Error the keyword cannot be empty\n".encode())
+                client_socket.send("Error the keyword cannot be empty\n".encode('utf-8'))
                 continue
 
             cur.execute("INSERT INTO Files (file_name, keyword, peer_id) VALUES (?, ?, ?)", (file_name, keyword_name, user_id))
 
-        client_socket.send("Are you finished sending files? Type Y/N: ".encode())
+        client_socket.send("Are you finished sending files? Type Y/N: ".encode('utf-8'))
 
         y_or_no = client_socket.recv(1024).decode().strip()
 
         if y_or_no.lower() == "y":
             cur.connection.commit()
-            client_socket.send("File upload completed.\n".encode())
+            client_socket.send("File upload completed.\n".encode('utf-8'))
             print("FILE UPLOAD SUCCESS")
             break
         else:
@@ -172,24 +126,24 @@ def upload_files(client_socket, cur, user_id):
 
 
 def user_query(client_socket, cur, user_id):
-    client_socket.send("How many files do you want to query?: ".encode())
+    client_socket.send("How many files do you want to query?: ".encode('utf-8'))
     number_of_files = client_socket.recv(1024).decode().strip()
 
     if not number_of_files.isdigit() or int(number_of_files) > 10:
-        client_socket.send("Error, enter a whole number less than 10\n".encode())
+        client_socket.send("Error, enter a whole number less than 10\n".encode('utf-8'))
         return
 
     number_of_files = int(number_of_files)
     keyword_list = []
 
     for i in range(1, number_of_files + 1):
-        client_socket.send(f"Enter the Keyword for file {i}: ".encode())
+        client_socket.send(f"Enter the Keyword for file {i}: ".encode('utf-8'))
         keyword = client_socket.recv(1024).decode().strip()
         if keyword:
             keyword_list.append(keyword)
 
     if not keyword_list:
-        client_socket.send("No valid keywords entered.\n".encode())
+        client_socket.send("No valid keywords entered.\n".encode('utf-8'))
         return
 
     table = PrettyTable(["File Name", "Keyword", "Domain Name", "Port", "Uploaded by", "RSA Public Key", "DSA Public Key"])
@@ -212,17 +166,44 @@ def user_query(client_socket, cur, user_id):
             table.add_row([None, keyword, None, None, "No results found", None, None])
 
     if any_results:
-        client_socket.send(b"\n" + table.get_string().encode() + b"\n\n")
+        client_socket.send(b"\n" + table.get_string().encode('utf-8') + b"\n\n")
     else:
-        client_socket.send("No results found for any keywords.\n".encode())
+        client_socket.send("No results found for any keywords.\n".encode('utf-8'))
 
     print(f"UserID: {user_id} Queried {number_of_files} {'files' if number_of_files > 1 else 'file'}")
 
 
-def handle_client(client_socket, user_id = None):
+def receive_full_message(sock):
+    sock.settimeout(0.1)
+    buffer = []
+    try:
+        while True:
+            part = sock.recv(1024)
+            if not part:
+                break
+            buffer.append(part)
+    except socket.timeout:
+        print("Socket timed out waiting for data.")
+    finally:
+        sock.settimeout(None)
+    return b''.join(buffer)
+
+def get_keys(client_socket):
+    RSA_key = client_socket.recv(1024).decode('utf-8')
+    client_socket.send("RSA Key Received".encode('utf-8'))
+    DSA_key = receive_full_message(client_socket).decode('utf-8')
+    client_socket.send("Both Keys Received".encode('utf-8'))
+    return RSA_key, DSA_key
+
+def handle_client(client_socket, user_id=None):
     conn = sqlite3.connect("indexing_server.db")
     cur = conn.cursor()
 
+    RSA_key, DSA_key = get_keys(client_socket)
+    if RSA_key is None or DSA_key is None:
+        print("Failed to get keys")
+        client_socket.close()
+        return
     try:
         menu = ("Welcome to Secure File Sharing\n"
                 "------------------------------\n"
@@ -231,18 +212,18 @@ def handle_client(client_socket, user_id = None):
                 "2 - Login\n")
 
         while True:
-            client_socket.send(menu.encode())
+            client_socket.send(menu.encode('utf-8'))
             message = client_socket.recv(1024).decode()
             if not message:
                 continue
             elif message == '1':
-                user_id = handle_registration(client_socket, cur)
+                user_id = handle_registration(client_socket, cur, RSA_key, DSA_key)
                 break
             elif message == '2':
-                user_id = handle_login(client_socket, cur)
+                user_id = handle_login(client_socket, cur, RSA_key, DSA_key)
                 break
             else:
-                client_socket.send("Please enter 1 or 2\n".encode())
+                client_socket.send("Please enter 1 or 2\n".encode('utf-8'))
                 continue
 
         menu_dashboard = ("\nWelcome to The Dashboard\n"
@@ -255,7 +236,7 @@ def handle_client(client_socket, user_id = None):
                 "5 - Quit\n")
 
         while True:
-            client_socket.send(menu_dashboard.encode())
+            client_socket.send(menu_dashboard.encode('utf-8'))
             message = client_socket.recv(1024).decode().strip()
             if message == '1':
                 domain_handle(client_socket, cur, user_id)
@@ -266,10 +247,10 @@ def handle_client(client_socket, user_id = None):
                 continue
 
             elif message == '3':
-                client_socket.send("Are you sure you want to send files? You will be disconnected from the indexing server Y/N".encode())
+                client_socket.send("Are you sure you want to send files? You will be disconnected from the indexing server Y/N".encode('utf-8'))
                 message = client_socket.recv(1024).decode().strip()
                 if message.lower() == 'y':
-                    client_socket.send("Entering File Send Mode".encode())
+                    client_socket.send("Entering File Send Mode".encode('utf-8'))
                     client_socket.close()
                     conn.close()
                     break
@@ -277,10 +258,10 @@ def handle_client(client_socket, user_id = None):
                     continue
 
             elif message == '4':
-                client_socket.send("Are you sure you want to request files? You will be disconnected from the indexing server Y/N".encode())
+                client_socket.send("Are you sure you want to request files? You will be disconnected from the indexing server Y/N".encode('utf-8'))
                 message = client_socket.recv(1024).decode().strip()
                 if message.lower() == 'y':
-                    client_socket.send("Entering File Request Mode".encode())
+                    client_socket.send("Entering File Request Mode".encode('utf-8'))
                     client_socket.close()
                     conn.close()
                     break
@@ -288,10 +269,10 @@ def handle_client(client_socket, user_id = None):
                     continue
 
             elif message == '5':
-                client_socket.send("__user_quits__".encode())
+                client_socket.send("__user_quits__".encode('utf-8'))
                 break
             else:
-                client_socket.send("Please enter 1 or 2\n".encode())
+                client_socket.send("Please enter 1 or 2\n".encode('utf-8'))
                 continue
     finally:
         print(f"\nUserID: {user_id} | Disconnected\n")
