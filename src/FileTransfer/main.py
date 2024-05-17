e1e8ec2817569f4628ce7408a7f16357bbef9b9b
NickPrivate <dragonwolf478@gmail.com>
2024-05-16 23:37:04 -0700

    Added feature to verify signature using RSA to ensure file integridy, added more error checking, fixed some bugs

--- src/FileTransfer/main.py
+++ src/FileTransfer/main.py
@@ -1,4 +1,5 @@
 import socket
+import time
 import os
 import tqdm
 
@@ -8,6 +9,42 @@ from cryptography.hazmat.primitives.asymmetric import rsa, dsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.exceptions import UnsupportedAlgorithm
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from cryptography.exceptions import InvalidSignature
+
+def sign_data(private_key, data):
+    if isinstance(data, str):
+        data = data.encode('utf-8')
+    
+    signature = private_key.sign(
+        data,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+def verify_signature(public_key, signature, data):
+    if isinstance(data, str):
+        data = data.encode('utf-8')
+    
+    try:
+        public_key.verify(
+            signature,
+            data,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return True
+    except InvalidSignature:
+        print("Verification failed: Invalid signature")
+    except Exception as e:
+        print(f"Verification failed: {e}")
+    return False
 
 def receive_full_message(sock):
     buffer = []
@@ -131,40 +168,72 @@ def sender():
     try:
         print(f"Connection from {addr}")
 
-        # 2 Receive the receivers public key
-        message = receiver_socket.recv(1024).decode()
+        # Receive the receivers public key
+        receivers_pub_key = receiver_socket.recv(1024).decode()
 
-        # 3 Generate AES KEY
+        # Generate AES KEY
         secure_aes_key, nonce = generate_aes_key_and_nonce()
 
-        # 4 Encrypt AES KEY and Send IT
-        encrypted_aes_key = encrypt_aes_key(secure_aes_key, nonce, message)
+        # Encrypt AES KEY and Send IT
+        encrypted_aes_key = encrypt_aes_key(secure_aes_key, nonce, receivers_pub_key)
         receiver_socket.send(encrypted_aes_key)
 
         print("Received RSA Key")
         print("Sending Encrypted AES Key...")
-        # 7 Receive Acknowledgement
+        # Receive Acknowledgement
         message = receiver_socket.recv(1024).decode()
 
-        print(message)
 
-        # 9 Get file name
-        file_name = receiver_socket.recv(1024).decode()
-        print(f"received file name{file_name}")
+        receiver_socket.send(RSApublic_key.encode())
+
+        print(message)
+    except:
+        print("Sender() Error with Key Transfer")
+        receiver_socket.close()
+        sender_socket.close()
+        exit()
+
+    try:
+        # Get file name
+        while True:
+            file_name = receiver_socket.recv(1024).decode()
+
+            if os.path.exists(file_name):
+                receiver_socket.send(str(1).encode())
+                break
+            else:
+                receiver_socket.send(str(0).encode())
+                print(f"Error: {file_name} does not exist")
+                continue
+
+        message = receiver_socket.recv(1024).decode()
+
+        print(f"Received file name: {file_name}")
         file_size = os.path.getsize(file_name)
+
         receiver_socket.send(str(file_size).encode())
-        # 10 store all of the file data in the variable
+
+        # Store all of the file data in the variable
         with open(file_name, "rb") as f:
             data = f.read()
 
         cipher = AES.new(secure_aes_key, AES.MODE_EAX, nonce)
         encrypted_file = cipher.encrypt(data)
 
-        # 11 Encrypt the file with AES and send it 
+        signature = sign_data(load_pem_private_key(RSAprivate_key.encode(), password=None), data)
+
+        # Encrypt the file with AES and send it 
+        receiver_socket.send(signature)
+
+        sig_received = receiver_socket.recv(1024).decode()
+        print(sig_received)
+
         receiver_socket.sendall(encrypted_file)
         receiver_socket.send(b"<END>")
-        print(encrypted_file)
-        print("This is the encrypted file ^")
+        print("Encrypted File Sent")
+
+    except:
+        print("Sender() Error Handling File")
 
     finally:
         receiver_socket.close()
@@ -179,38 +248,62 @@ def receiver():
     try:
         receiver_socket.connect((domain_name, port))
 
-        # 1 Send RSA Public key
+        # Send RSA Public key
         receiver_socket.send(RSApublic_key.encode())
-        # 5 Receive encrypted AES Key
+        # Receive encrypted AES Key
         encrypted_aes_key = receiver_socket.recv(1024)
 
-        # 6 Send Acknowledgement
+        # Send Acknowledgement
         receiver_socket.send("Successfully received AES Key".encode())
 
+        # Receive Senders Public Key
+        senders_public_key = receiver_socket.recv(1024)
         secure_aes_key, nonce = decrypt_aes_key(encrypted_aes_key, RSAprivate_key)
 
-        print(secure_aes_key)
         cipher = AES.new(secure_aes_key, AES.MODE_EAX, nonce)
         print("AES Key secure transmission complete")
 
-        # 8 Send file name to sender
-        file_name = input("Enter the name of the file you want to download:").strip()
-        receiver_socket.send(file_name.encode())
+    except:
+        print("Receiver() Error with Key Transfer")
+        receiver_socket.close()
+        exit()
 
-        # 12 receive the file name and size
+    try:
+
+        # Send file name to sender
+        while True:
+            file_name = input("Enter the name of the file you want to download:").strip()
+            receiver_socket.send(file_name.encode())
+
+            file_exists = receiver_socket.recv(1024).decode()
+            if file_exists != "0":
+                break
+            else:
+                print("Error: No file found, try again")
+                continue
+
+        receiver_socket.send("Success, File Found".encode())
+
+        # Receive the file name and size
         file_size = receiver_socket.recv(1024).decode()
-        print(file_name)
-        print(file_size)
+        print(f"The file size is {file_size} Bytes")
 
-        file_name = "Sent" + file_name
+        file_name = "Received" + file_name
         file = open(file_name, "wb")
-        print("File is opened")
 
         done = False
 
         file_bytes = b""
 
-        # 13 receive all of the bytes of the file
+        # Receive the signature
+        signature = receiver_socket.recv(256)
+
+        receiver_socket.send("Signature received".encode())
+
+        done = False
+
+        # Receive all of the bytes of the file
+        print(f"File {file_name} is being received...")
         progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000, total=int(file_size))
         while not done:
             data = receiver_socket.recv(1024)
@@ -219,10 +312,21 @@ def receiver():
             else:
                 file_bytes += data
             progress.update(1024)
-        print(file_bytes)
-        # 14 decrypt the file
-        file.write(cipher.decrypt(file_bytes[:-5]))
+
+        decrypt = cipher.decrypt(file_bytes[:-5])
+
+        # Verify the signature
+        print("Verifying Signature...")
+        if verify_signature(serialization.load_pem_public_key(senders_public_key), signature, decrypt):
+            print("RSA Signature verified successfully")
+            file.write(decrypt)
+            print("Success, File Transfer Complete")
+        else:
+            print("Signature verification failed")
+            print("Fatal error, canceling file transfer") 
+
         file.close()
+
 
     except:
         print("Receiver Closed Unexpectedly")
@@ -241,6 +345,7 @@ while True:
     if choice == '1':
         sender()
         print("THAT'S ALL FOLKS")
+        time.sleep(20)
         exit()
     elif choice == '2':
         receiver()
